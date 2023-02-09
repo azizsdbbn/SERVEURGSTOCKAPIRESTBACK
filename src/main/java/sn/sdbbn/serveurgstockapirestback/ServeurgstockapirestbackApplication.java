b5df6216e97c234016d4f8de3d451a07eea5d56d
Abdou@sdbbn <94757744+sdbbn@users.noreply.github.com>
2023-02-09 15:30:38 +0000

    first commit

---
+++ src/main/java/sn/sdbbn/serveurgstockapirestback/ServeurgstockapirestbackApplication.java
@@ -0,0 +1,13 @@
+package sn.sdbbn.serveurgstockapirestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurgstockapirestbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeurgstockapirestbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/sdbbn/serveurgstockapirestback/ServeurgstockapirestbackApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.sdbbn.serveurgstockapirestback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurgstockapirestbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
